d7c54ce5a222a92fa399e2525b09c083cf81a6a6
emanuelhub-dev <emanuelfranco8816@gmail.com>
2026-06-10 14:56:29 -0300

    Add files via upload

---
+++ site copa do mundo/script.js
@@ -0,0 +1,925 @@
+const matches = [
+
+  
+// 11 JUNHO
+
+  {
+    date:'11 Junho',
+    time:'16:00',
+    home:'México',
+    away:'Africa do Sul'
+  },
+
+
+  {
+    date:'11 Junho',
+    time:'23:00',
+    home:'Coreia do Sul',
+    away:'Tchéquia'
+  },
+
+
+
+  // 12 JUNHO
+
+  {
+    date:'12 Junho',
+    time:'16:00',
+    home:'Canadá',
+    away:'Bósnia'
+  },
+
+  {
+    date:'12 Junho',
+    time:'22:00',
+    home:'Estados Unidos',
+    away:'Paraguai'
+  },
+
+
+
+  // 13 JUNHO
+
+  {
+    date:'13 Junho',
+    time:'22:00',
+    home:'Haiti',
+    away:'Escócia'
+  },
+
+  {
+    date:'13 Junho',
+    time:'01:00',
+    home:'Austrália',
+    away:'Turquia'
+  },
+
+  {
+    date:'13 Junho',
+    time:'19:00',
+    home:'Brasil',
+    away:'Marrocos'
+  },
+
+  {
+    date:'13 Junho',
+    time:'16:00',
+    home:'Catar',
+    away:'Suíça'
+  },
+
+
+
+  // 14 JUNHO
+
+  {
+    date:'14 Junho',
+    time:'20:00',
+    home:'Costa do Marfim',
+    away:'Equador'
+  },
+
+  {
+    date:'14 Junho',
+    time:'14:00',
+    home:'Alemanha',
+    away:'Curaçao'
+  },
+
+  {
+    date:'14 Junho',
+    time:'17:00',
+    home:'Holanda',
+    away:'Japão'
+  },
+
+  {
+    date:'14 Junho',
+    time:'23:00',
+    home:'Suécia',
+    away:'Tunísia'
+  },
+
+
+
+  // 15 JUNHO
+
+  {
+    date:'15 Junho',
+    time:'13:00',
+    home:'Espanha',
+    away:'Cabo Verde'
+  },
+
+  {
+    date:'15 Junho',
+    time:'16:00',
+    home:'Bélgica',
+    away:'Egito'
+  },
+
+  {
+    date:'15 Junho',
+    time:'19:00',
+    home:'Arábia Saudita',
+    away:'Uruguai'
+  },
+
+  {
+    date:'15 Junho',
+    time:'22:00',
+    home:'Irã',
+    away:'Nova Zelândia'
+  },
+
+
+
+  // 16 JUNHO
+
+  {
+    date:'16 Junho',
+    time:'16:00',
+    home:'França',
+    away:'Senegal'
+  },
+
+  {
+    date:'16 Junho',
+    time:'19:00',
+    home:'Iraque',
+    away:'Noruega'
+  },
+
+  {
+    date:'16 Junho',
+    time:'22:00',
+    home:'Argentina',
+    away:'Argélia'
+  },
+
+  {
+    date:'16 Junho',
+    time:'01:00',
+    home:'Áustria',
+    away:'Jordânia'
+  },
+
+
+
+  // 17 JUNHO
+
+  {
+    date:'17 Junho',
+    time:'14:00',
+    home:'Portugal',
+    away:'RD Congo'
+  },
+
+  {
+    date:'17 Junho',
+    time:'17:00',
+    home:'Inglaterra',
+    away:'Croácia'
+  },
+
+  {
+    date:'17 Junho',
+    time:'20:00',
+    home:'Gana',
+    away:'Panamá'
+  },
+
+  {
+    date:'17 Junho',
+    time:'23:00',
+    home:'Uzbequistão',
+    away:'Colômbia'
+  },
+
+
+
+  // 18 JUNHO
+
+  {
+    date:'18 Junho',
+    time:'13:00',
+    home:'Tchéquia',
+    away:'África do Sul'
+  },
+
+  {
+    date:'18 Junho',
+    time:'22:00',
+    home:'México',
+    away:'Coreia do Sul'
+  },
+
+   {
+    date:'18 Junho',
+    time:'16:00',
+    home:'Suíça',
+    away:'Bósnia'
+  },
+
+    {
+    date:'18 Junho',
+    time:'19:00',
+    home:'Canadá',
+    away:'Catar'
+  },
+
+
+
+  // 19 JUNHO
+
+  {
+    date:'19 Junho',
+    time:'16:00',
+    home:'Estados Unidos',
+    away:'Austrália'
+  },
+
+  {
+    date:'19 Junho',
+    time:'00:00',
+    home:'Turquia',
+    away:'Paraguai'
+  },
+
+    {
+    date:'19 Junho',
+    time:'19:00',
+    home:'Escócia',
+    away:'Marrocos'
+  },
+
+  {
+    date:'19 Junho',
+    time:'21:30',
+    home:'Brasil',
+    away:'Haiti'
+  },
+
+  // 20 JUNHO
+
+  {
+    date:'20 Junho',
+    time:'17:00',
+    home:'Alemanha',
+    away:'Costa do Marfim'
+  },
+
+  {
+    date:'20 Junho',
+    time:'21:00',
+    home:'Equador',
+    away:'Curaçao'
+  },
+
+    {
+    date:'20 Junho',
+    time:'14:00',
+    home:'Holanda',
+    away:'Suécia'
+  },
+
+  {
+    date:'20 Junho',
+    time:'23:00',
+    home:'Tunísia',
+    away:'Japão'
+  },
+
+
+
+  // 21 JUNHO
+
+  {
+    date:'21 Junho',
+    time:'16:00',
+    home:'Bélgica',
+    away:'Irã'
+  },
+
+  {
+    date:'21 Junho',
+    time:'22:00',
+    home:'Nova Zelândia',
+    away:'Egito'
+  },
+
+    {
+    date:'21 Junho',
+    time:'13:00',
+    home:'Espanha',
+    away:'Arábia Saudita'
+  },
+
+  {
+    date:'21 Junho',
+    time:'19:00',
+    home:'Uruguai',
+    away:'Cabo Verde'
+  },
+
+
+
+  // 22 JUNHO
+
+
+  {
+    date:'22 Junho',
+    time:'18:00',
+    home:'França',
+    away:'Iraque'
+  },
+
+  {
+    date:'22 Junho',
+    time:'21:00',
+    home:'Noruega',
+    away:'Senegal'
+  },
+
+    {
+    date:'22 Junho',
+    time:'14:00',
+    home:'Argentina',
+    away:'Áustria'
+  },
+
+
+  // 23 JUNHO
+
+
+  {
+    date:'23 Junho',
+    time:'00:00',
+    home:'Jordânia',
+    away:'Argélia'
+  },
+
+  {
+    date:'23 Junho',
+    time:'14:00',
+    home:'Portugal',
+    away:'Uzbequistão'
+  },
+
+  {
+    date:'23 Junho',
+    time:'23:00',
+    home:'Colômbia',
+    away:'RD Congo'
+  },
+
+    {
+    date:'23 Junho',
+    time:'17:00',
+    home:'Inglaterra',
+    away:'Gana'
+  },
+
+    {
+    date:'23 Junho',
+    time:'20:00',
+    home:'Panamá',
+    away:'Croácia'
+  },
+
+
+
+  // 24 JUNHO
+
+ {
+    date:'24 Junho',
+    time:'16:00',
+    home:'Suíça',
+    away:'Canadá'
+  },
+
+  {
+    date:'24 Junho',
+    time:'16:00',
+    home:'Bósnia',
+    away:'Catar'
+  },
+
+  {
+    date:'24 Junho',
+    time:'19:00',
+    home:'Escócia',
+    away:'Brasil'
+  },
+
+  {
+    date:'24 Junho',
+    time:'19:00',
+    home:'Marrocos',
+    away:'Haiti'
+  },
+
+  {
+    date:'24 Junho',
+    time:'22:00',
+    home:'Tchéquia',
+    away:'México'
+  },
+
+  {
+    date:'24 Junho',
+    time:'22:00',
+    home:'África do Sul',
+    away:'Coreia do Sul'
+  },
+
+
+
+  // 25 JUNHO
+
+  {
+    date:'25 Junho',
+    time:'17:00',
+    home:'Curaçao',
+    away:'Costa do Marfim'
+  },
+
+  {
+    date:'25 Junho',
+    time:'17:00',
+    home:'Equador',
+    away:'Alemanha'
+  },
+
+    {
+    date:'25 Junho',
+    time:'20:00',
+    home:'Japão',
+    away:'Suécia'
+  },
+
+  {
+    date:'25 Junho',
+    time:'20:00',
+    home:'Tunisia',
+    away:'Holanda'
+  },
+
+      {
+    date:'25 Junho',
+    time:'23:00',
+    home:'Turquia',
+    away:'Estados Unidos'
+  },
+
+  {
+    date:'25 Junho',
+    time:'23:00',
+    home:'Paraguai',
+    away:'Austrália'
+  },
+
+
+
+  // 26 JUNHO
+  
+    {
+    date:'26 Junho',
+    time:'16:00',
+    home:'Noruega',
+    away:'França'
+  },
+
+  {
+    date:'26 Junho',
+    time:'16:00',
+    home:'Senegal',
+    away:'Iraque'
+  },
+
+    {
+    date:'26 Junho',
+    time:'21:00',
+    home:'Uruguai',
+    away:'Espanha'
+  },
+
+  {
+    date:'26 Junho',
+    time:'21:00',
+    home:'Cabo Verde',
+    away:'Arábia Saudita'
+  },
+
+
+
+
+
+
+
+
+  // 27 JUNHO
+
+  {
+    date:'27 Junho',
+    time:'00:00',
+    home:'Egito',
+    away:'Irã'
+  },
+
+  {
+    date:'27 Junho',
+    time:'00:00',
+    home:'Nova Zelândia',
+    away:'Bélgica'
+  },
+
+
+  {
+    date:'27 Junho',
+    time:'23:00',
+    home:'Argélia',
+    away:'Áustria'
+  },
+
+  {
+    date:'27 Junho',
+    time:'23:00',
+    home:'Jordânia',
+    away:'Argentina'
+  },
+
+  {
+    date:'27 Junho',
+    time:'20:30',
+    home:'Colômbia',
+    away:'Portugal'
+  },
+
+  {
+    date:'27 Junho',
+    time:'20:30',
+    home:'RD Congo',
+    away:'Uzbequistão'
+  },
+
+];
+
+const matchesContainer = document.getElementById('matchesContainer');
+const goalsTable = document.getElementById('goalsTable');
+const assistsTable = document.getElementById('assistsTable');
+
+let scorers = JSON.parse(localStorage.getItem('scorers')) || {};
+let assists = JSON.parse(localStorage.getItem('assists')) || {};
+let results = JSON.parse(localStorage.getItem('results')) || {};
+let matchDetails = JSON.parse(
+  localStorage.getItem('matchDetails')
+) || {};
+let savedMatches = JSON.parse(localStorage.getItem('savedMatches')) || {};
+
+function renderMatches() {
+
+  matchesContainer.innerHTML = '';
+
+  const groupedMatches = {};
+
+  matches.forEach(match => {
+
+    if (!groupedMatches[match.date]) {
+      groupedMatches[match.date] = [];
+    }
+
+    groupedMatches[match.date].push(match);
+
+  });
+
+  let globalIndex = 0;
+
+  Object.keys(groupedMatches).forEach((date, dayIndex) => {
+
+  const daySection = document.createElement('div');
+
+daySection.innerHTML = `
+<h2 class="day-title">
+  <span class="arrow">
+    ${dayIndex === 0 ? '▼' : '▶'}
+  </span>
+  📅 ${date}
+  <span class="games-count">
+    • ${groupedMatches[date].length} jogos
+  </span>
+</h2>
+<div class="day-matches"></div>
+`;
+
+    const dayMatchesContainer =
+      daySection.querySelector('.day-matches');
+     if (dayIndex > 0) {
+  dayMatchesContainer.style.display = 'none';
+} 
+
+    groupedMatches[date].forEach(match => {
+
+      const card = document.createElement('div');
+
+      card.classList.add('match-card');
+      
+      card.innerHTML = `
+
+      ${savedMatches[globalIndex]
+  ? '<div class="finished-badge">✓ Finalizada</div>'
+  : ''
+}
+      
+        <div class="match-header">
+
+          <div class="match-date">
+            ${match.date}
+          </div>
+
+          <div class="match-time">
+            ${match.time}
+          </div>
+
+        </div>
+
+        <div class="teams">
+
+          <div class="team">${match.home}</div>
+
+          <div class="score">
+
+            <input
+              type="number"
+              min="0"
+              value="${results[globalIndex]?.home || 0}"
+              id="home-score-${globalIndex}"
+              class="score-input"
+            >
+
+            <span>x</span>
+
+            <input
+              type="number"
+              min="0"
+              value="${results[globalIndex]?.away || 0}"
+              id="away-score-${globalIndex}"
+              class="score-input"
+            >
+
+          </div>
+
+          <div class="team">${match.away}</div>
+
+        </div>
+
+        ${!savedMatches[globalIndex] ? `
+
+<div class="inputs">
+
+  <input
+    type="text"
+    id="goal-${globalIndex}"
+    placeholder="Quem fez os gols?"
+  >
+
+  <input
+    type="text"
+    id="assist-${globalIndex}"
+    placeholder="Quem deu assistência?"
+  >
+
+</div>
+
+` : ''}
+
+        ${matchDetails[globalIndex] ? `
+
+<div class="match-details">
+
+  <div class="goals-list">
+    ⚽ ${matchDetails[globalIndex].goals.join(', ')}
+  </div>
+
+  <div class="assists-list">
+    🎯 ${matchDetails[globalIndex].assists.join(', ')}
+  </div>
+
+</div>
+
+` : ''}
+    ${!savedMatches[globalIndex] ? `
+
+<button
+  class="save-btn"
+  data-index="${globalIndex}"
+>
+  Salvar Estatísticas
+</button>
+
+` : ''}
+      `;
+
+      dayMatchesContainer.appendChild(card);
+
+      globalIndex++;
+
+    });
+
+    matchesContainer.appendChild(daySection);
+  const title =
+  daySection.querySelector('.day-title');
+
+const matchesDiv =
+  daySection.querySelector('.day-matches');
+
+if (dayIndex === 0) {
+
+  matchesDiv.style.display = 'grid';
+
+} else {
+
+  matchesDiv.style.display = 'none';
+
+}
+
+title.addEventListener('click', () => {
+
+  const arrow =
+    title.querySelector('.arrow');
+
+  if (matchesDiv.style.display === 'none') {
+
+    matchesDiv.style.display = 'grid';
+
+    arrow.textContent = '▼';
+
+  } else {
+
+    matchesDiv.style.display = 'none';
+
+    arrow.textContent = '▶';
+
+  }
+
+});
+  });
+
+  const buttons =
+    document.querySelectorAll('.save-btn');
+
+  buttons.forEach(button => {
+
+    button.addEventListener('click', () => {
+
+      const index = button.dataset.index;
+
+      saveMatch(index);
+
+    });
+
+  });
+
+}
+
+function saveMatch(index) {
+
+  if (savedMatches[index]) {
+
+  alert('Essa partida já foi contabilizada.');
+
+  return;
+}
+
+  const goalInput = document.getElementById(`goal-${index}`).value;
+
+  const assistInput = document.getElementById(`assist-${index}`).value;
+
+  const homeScore =
+document.getElementById(`home-score-${index}`).value;
+
+const awayScore =
+document.getElementById(`away-score-${index}`).value;
+
+  const goalPlayers = goalInput
+    .split(',')
+    .map(player => player.trim())
+    .filter(player => player !== '');
+
+  const assistPlayers = assistInput
+    .split(',')
+    .map(player => player.trim())
+    .filter(player => player !== '');
+
+  goalPlayers.forEach((player) => {
+
+    scorers[player] = (scorers[player] || 0) + 1;
+
+  });
+
+  assistPlayers.forEach((player) => {
+
+    assists[player] = (assists[player] || 0) + 1;
+
+  });
+
+  localStorage.setItem('scorers', JSON.stringify(scorers));
+
+  localStorage.setItem('assists', JSON.stringify(assists));
+
+  results[index] = {
+
+  home: homeScore,
+
+  away: awayScore
+
+};
+
+matchDetails[index] = {
+
+  goals: goalPlayers,
+
+  assists: assistPlayers
+
+};
+
+localStorage.setItem(
+  'matchDetails',
+  JSON.stringify(matchDetails)
+);
+
+localStorage.setItem(
+  'results',
+  JSON.stringify(results)
+);
+
+savedMatches[index] = true;
+
+localStorage.setItem(
+  'savedMatches',
+  JSON.stringify(savedMatches)
+);
+
+renderMatches();
+
+  renderTables();
+
+  alert('Estatísticas salvas!');
+
+}
+
+function renderTables() {
+
+  goalsTable.innerHTML = '';
+
+  assistsTable.innerHTML = '';
+
+  Object.entries(scorers)
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([player, goals]) => {
+
+      goalsTable.innerHTML += `
+        <tr>
+          <td>${player}</td>
+          <td>${goals}</td>
+        </tr>
+      `;
+
+    });
+
+  Object.entries(assists)
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([player, total]) => {
+
+      assistsTable.innerHTML += `
+        <tr>
+          <td>${player}</td>
+          <td>${total}</td>
+        </tr>
+      `;
+
+      });
+
+      function limparDados() {
+
+  localStorage.clear();
+
+  location.reload();
+
+}
+}
+
+
+renderMatches();
+
+renderTables();
+
+document
+  .getElementById("clearStats")
+  .addEventListener("click", () => {
+
+    localStorage.clear();
+
+    location.reload();
+
+  });
